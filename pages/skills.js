6aba146b878056be0a741f39b43b16ed4720b964
Piotr <piotr.bania@europe.com>
2023-05-02 12:18:23 +0100

    init

---
+++ pages/skills.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Head from 'next/head'
+import { motion as m } from 'framer-motion'
+import Experience from '../components/homepage/Experience'
+
+export default function Skills() {
+
+    return (
+        <m.div
+            initial={{opacity: 0, transition: {duration: 1}}}
+            animate={{opacity: 1, transition: {delay: 1, duration: 1}}}
+            exit={{opacity: 0, transition: {duration: 1}}}
+            transition={{duration: 1, ease: 'easeOut', delay: 0}}
+            >
+            <Head>
+                <title>Piotr Bania | Folio</title>
+                <meta name="description" content="Piotr Bania | Folio" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="./svg/logo.svg" />
+            </Head>
+
+            <main>
+                <Experience />
+            </main>
+        </m.div>
+    )
+}
