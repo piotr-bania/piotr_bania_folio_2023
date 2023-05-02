96bb76805aebe601753fd66cc63bc7e70e59a05b
Piotr <piotr.bania@europe.com>
2023-05-02 13:02:15 +0100

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import Layout from '../components/Layout'
 
 import '../styles/globals.scss'
 import '../styles/header.scss'
-import '../styles/homepage_experience.scss'
+import '../styles/homepage.scss'
 
 function MyApp({ Component, pageProps }) {
     return (
